1395abde3b332178c3112d9fc6b044344fff6630
Charan Teja <charanteja@Charans-MacBook-Pro.local>
2025-11-09 11:49:42 +0530

    skeleton

---
+++ src/main/java/com/divinova/pooja_master_backend_service/PoojaMasterBackendServiceApplication.java
@@ -0,0 +1,13 @@
+package com.divinova.pooja_master_backend_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoojaMasterBackendServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoojaMasterBackendServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/divinova/pooja_master_backend_service/PoojaMasterBackendServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.divinova.pooja_master_backend_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoojaMasterBackendServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
